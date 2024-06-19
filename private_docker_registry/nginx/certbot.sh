14779da55e798be324b067cf04f48ddecb7b50e8
aby <anti1346@gmail.com>
2024-06-19 22:18:19 +0900

    no message

--- private_docker_registry/nginx/certbot.sh
+++ private_docker_registry/nginx/certbot.sh
@@ -47,6 +47,9 @@ else
     echo "디렉토리가 이미 존재합니다: $CERTBOT_CONFIG_DIR"
 fi
 
+# 절대 경로로 변환
+CERTBOT_CONFIG_DIR=$(realpath "$CERTBOT_CONFIG_DIR")
+
 # Docker 컨테이너 실행을 통해 Let's Encrypt 인증서 발급 및 갱신
 docker run -it --rm --name certbot \
     -v "$CERTBOT_CONFIG_DIR:/etc/letsencrypt" \
