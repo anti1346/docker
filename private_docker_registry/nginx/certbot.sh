d42d288a79488c9dbb770b9206382aba0bfdcf07
anti1346 <anti1346@mt.co.kr>
2024-06-19 13:34:26 +0900

    private docker registry

---
+++ private_docker_registry/nginx/certbot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 필요한 경로를 변수로 설정
+CERTBOT_ETC_DIR='/docker_container/docker_registry/nginx/letsencrypt'
+
+# 도메인과 이메일 주소 설정
+DOMAIN='registry.sangchul.kr'
+EMAIL='admin@sangchul.kr'
+
+# Docker 컨테이너 실행을 통해 Let's Encrypt 인증서 발급 및 갱신
+docker run -it --rm --name certbot \
+    -v "$CERTBOT_ETC_DIR:/etc/letsencrypt" \
+    certbot/certbot certonly \
+    -d "$DOMAIN" \
+    --email "$EMAIL" \
+    --manual --preferred-challenges dns \
+    --agree-tos
