433fc32270c670f497089c14a120746064d3198e
Aby <42926329+anti1346@users.noreply.github.com>
2024-06-19 21:42:41 +0900

    Update certbot.sh

--- private_docker_registry/nginx/certbot.sh
+++ private_docker_registry/nginx/certbot.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # 필요한 경로를 변수로 설정
-CERTBOT_ETC_DIR='/docker_container/docker_registry/nginx/letsencrypt'
+CERTBOT_ETC_DIR='letsencrypt'
 
 # 도메인과 이메일 주소 설정
 DOMAIN='registry.sangchul.kr'
 EMAIL='admin@sangchul.kr'
+
+# letsencrypt 디렉토리 생성
+mkdir $CERTBOT_ETC_DIR
 
 # Docker 컨테이너 실행을 통해 Let's Encrypt 인증서 발급 및 갱신
 docker run -it --rm --name certbot \
